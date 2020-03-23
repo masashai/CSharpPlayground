7980c9f6b1b24be6f85dd9aa97b22bd50b681fde
政史 前田 <maechan0710@gmail.com>
2020-03-23 23:03:24 +0900

    Added projects.

---
+++ CSharpPlayground.Lib/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace CSharpPlayground.Lib
+{
+    public static class Extensions
+    {
+        public static int LengthInTextElements(this String str)
+        {
+            var stringInfo = new StringInfo(str);
+            return stringInfo.LengthInTextElements;
+        }
+
+        public static String SubstringByTextElements(this String str, int index)
+        {
+            var stringInfo = new StringInfo(str);
+            return stringInfo.SubstringByTextElements(index);
+        }
+
+        public static String SubstringByTextElements(this String str, int index, int length)
+        {
+            var stringInfo = new StringInfo(str);
+            return stringInfo.SubstringByTextElements(index, length);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CSharpPlayground.Lib;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "好きな食べ物は𩸽（ほっけ）です。";
+            Console.WriteLine(String.Format("text: {0}, Length: {1}, LengthInTextElements: {2}", text, text.Length, text.LengthInTextElements()));
+        }
+    }
+}
