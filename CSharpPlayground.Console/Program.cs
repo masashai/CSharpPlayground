09c4fb60fc58d25a231665b2c381e1042fc34dea
政史 前田 <maechan0710@gmail.com>
2020-03-23 21:15:15 +0900

    プロジェクト ファイルを追加します。

---
+++ CSharpPlayground.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPlayground.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
